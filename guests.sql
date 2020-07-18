fbadfaae4889821dee5461f2e2ed7a84c68350d9
neelkanthk <neelkanthkaushik2014@gmail.com>
2020-07-19 00:12:46 +0530

    added .sql file

---
+++ guests.sql
@@ -0,0 +1,56 @@
+create table guests (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	country VARCHAR(50)
+);
+insert into guests (id, first_name, last_name, country) values (1, 'Mitch', 'Crevagh', 'France');
+insert into guests (id, first_name, last_name, country) values (2, 'Gibb', 'Semaine', 'France');
+insert into guests (id, first_name, last_name, country) values (3, 'Bee', 'Lafrentz', 'France');
+insert into guests (id, first_name, last_name, country) values (4, 'Wallie', 'Worcs', 'France');
+insert into guests (id, first_name, last_name, country) values (5, 'Case', 'Barkworth', 'France');
+insert into guests (id, first_name, last_name, country) values (6, 'Meredith', 'Ahren', 'France');
+insert into guests (id, first_name, last_name, country) values (7, 'Blinni', 'Lapish', 'United Kingdom');
+insert into guests (id, first_name, last_name, country) values (8, 'Lavena', 'Menego', 'France');
+insert into guests (id, first_name, last_name, country) values (9, 'Adrea', 'Kinnard', 'France');
+insert into guests (id, first_name, last_name, country) values (10, 'Donica', 'Lafflin', 'France');
+insert into guests (id, first_name, last_name, country) values (11, 'Leodora', 'Witherop', 'France');
+insert into guests (id, first_name, last_name, country) values (12, 'Karola', 'Dumbrill', 'France');
+insert into guests (id, first_name, last_name, country) values (13, 'Mireielle', 'Urlin', 'France');
+insert into guests (id, first_name, last_name, country) values (14, 'West', 'Filyaev', 'United Kingdom');
+insert into guests (id, first_name, last_name, country) values (15, 'Dix', 'Warratt', 'France');
+insert into guests (id, first_name, last_name, country) values (16, 'Felita', 'Dawidowitsch', 'France');
+insert into guests (id, first_name, last_name, country) values (17, 'Piotr', 'Scading', 'France');
+insert into guests (id, first_name, last_name, country) values (18, 'Kelby', 'Ornelas', 'France');
+insert into guests (id, first_name, last_name, country) values (19, 'Dudley', 'Verbruggen', 'France');
+insert into guests (id, first_name, last_name, country) values (20, 'Skipp', 'Fletcher', 'France');
+insert into guests (id, first_name, last_name, country) values (21, 'Inger', 'Arboin', 'France');
+insert into guests (id, first_name, last_name, country) values (22, 'Valina', 'Pringley', 'France');
+insert into guests (id, first_name, last_name, country) values (23, 'Anitra', 'Fibbit', 'France');
+insert into guests (id, first_name, last_name, country) values (24, 'Sumner', 'Godfroy', 'France');
+insert into guests (id, first_name, last_name, country) values (25, 'Pamelina', 'Lynam', 'France');
+insert into guests (id, first_name, last_name, country) values (26, 'Prescott', 'Robbeke', 'France');
+insert into guests (id, first_name, last_name, country) values (27, 'Desi', 'Linbohm', 'France');
+insert into guests (id, first_name, last_name, country) values (28, 'Denise', 'Wythe', 'France');
+insert into guests (id, first_name, last_name, country) values (29, 'Eolande', 'Tunstall', 'France');
+insert into guests (id, first_name, last_name, country) values (30, 'Malvin', 'Domenico', 'France');
+insert into guests (id, first_name, last_name, country) values (31, 'Sonia', 'Walford', 'France');
+insert into guests (id, first_name, last_name, country) values (32, 'Franky', 'Capozzi', 'France');
+insert into guests (id, first_name, last_name, country) values (33, 'L;urette', 'Pennicott', 'France');
+insert into guests (id, first_name, last_name, country) values (34, 'Mirilla', 'Avramovsky', 'France');
+insert into guests (id, first_name, last_name, country) values (35, 'Aura', 'Caitlin', 'France');
+insert into guests (id, first_name, last_name, country) values (36, 'Doris', 'Fosse', 'France');
+insert into guests (id, first_name, last_name, country) values (37, 'Gayle', 'Groundwator', 'France');
+insert into guests (id, first_name, last_name, country) values (38, 'Angelina', 'Siuda', 'France');
+insert into guests (id, first_name, last_name, country) values (39, 'Hillary', 'Agg', 'France');
+insert into guests (id, first_name, last_name, country) values (40, 'Charisse', 'McAllester', 'France');
+insert into guests (id, first_name, last_name, country) values (41, 'Francine', 'Lantiffe', 'France');
+insert into guests (id, first_name, last_name, country) values (42, 'Beth', 'Colnet', 'France');
+insert into guests (id, first_name, last_name, country) values (43, 'August', 'Gelling', 'France');
+insert into guests (id, first_name, last_name, country) values (44, 'Saleem', 'Kneel', 'France');
+insert into guests (id, first_name, last_name, country) values (45, 'Ginevra', 'Chaffyn', 'France');
+insert into guests (id, first_name, last_name, country) values (46, 'Reeva', 'Messenbird', 'United Kingdom');
+insert into guests (id, first_name, last_name, country) values (47, 'Bryana', 'Lewendon', 'France');
+insert into guests (id, first_name, last_name, country) values (48, 'Leda', 'Elderbrant', 'France');
+insert into guests (id, first_name, last_name, country) values (49, 'Tye', 'Lifsey', 'France');
+insert into guests (id, first_name, last_name, country) values (50, 'Amalie', 'Neeves', 'France');
