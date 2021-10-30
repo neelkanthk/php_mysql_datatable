130658faf0f6c42d9eabdc8053d892ad6c9ccd26
neelkanthk <neelkanth2009@gmail.com>
2021-10-31 02:35:33 +0530

    Code improvement

--- guests.php
+++ guests.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 
 /**
@@ -21,8 +21,22 @@ if ($conn->connect_error) {
 
 getUsers($servername, $username, $password, $dbname, $conn);
 
+/**
+ * Get Users
+ *
+ * @param string] $servername
+ * @param string] $username
+ * @param string] $password
+ * @param string] $dbname
+ * @param $conn
+ * @return void
+ */
 function getUsers($servername, $username, $password, $dbname, $conn)
 {
+    if($servername . $username .$password . $dbname == "")
+    {
+        return false;
+    }
     //Get the page size and page number from the Datatables from client side
     $offset = !empty($_GET["start"]) ? filter_input(INPUT_GET, "start") : 0;
     $limit = !empty($_GET["length"]) ? filter_input(INPUT_GET, "length") : 10;
@@ -37,34 +51,40 @@ function getUsers($servername, $username, $password, $dbname, $conn)
         $query .= " WHERE first_name LIKE '%" . $searchTerm . "%' OR last_name LIKE '%" . $searchTerm . "%'";
     }
     $query .= " LIMIT $limit OFFSET $offset";
-    //Execute query an save result set in $results
-    $result = $conn->query($query);
-    $users = array();
-    if ($result->num_rows > 0) {
-        //Loop through the result set
-        while ($row = $result->fetch_assoc()) {
-            //Prepare the data in $users
-            array_push($users, [
-                "id" => $row["id"],
-                "firstname" => $row["first_name"],
-                "lastname" => $row["last_name"],
-                "country" => $row["country"]
-            ]);
+    try {
+        //Execute query an save result set in $results
+        $result = $conn->query($query);
+        $users = array();
+        if ($result->num_rows > 0) {
+            //Loop through the result set
+            while ($row = $result->fetch_assoc()) {
+                //Prepare the data in $users
+                array_push($users, [
+                    "id" => $row["id"],
+                    "firstname" => $row["first_name"],
+                    "lastname" => $row["last_name"],
+                    "country" => $row["country"]
+                ]);
+            }
         }
+        //Get total rows present in the table in $recordsTotal
+        $recordsTotal = $conn->query("SELECT count(*) FROM $table")->fetch_row();
+
+        //Get the total rows present in the table after applying filters in $recordsFiltered
+        $countFiltered = "SELECT count(*) FROM $table";
+        if (!empty($searchTerm)) {
+            $countFiltered .= " WHERE first_name LIKE '%" . $searchTerm . "%' OR last_name LIKE '%" . $searchTerm . "%'";
+        }
+        $recordsFiltered = $conn->query($countFiltered)->fetch_row();
+
+    } catch (\Throwable $th) {
+        throw $th;
     }
-    //Get total rows present in the table in $recordsTotal
-    $recordsTotal = $conn->query("SELECT count(*) FROM $table")->fetch_row();
-
-    //Get the total rows present in the table after applying filters in $recordsFiltered
-    $countFiltered = "SELECT count(*) FROM $table";
-    if (!empty($searchTerm)) {
-        $countFiltered .= " WHERE first_name LIKE '%" . $searchTerm . "%' OR last_name LIKE '%" . $searchTerm . "%'";
+    finally {
+        //Close the connection after MySQL work is done
+        $conn->close();
     }
-    $recordsFiltered = $conn->query($countFiltered)->fetch_row();
-
-    //Close the connection after MySQL work is done
-    $conn->close();
-
+    
     //Prepare data as required by the Datatables plugin.
     $data = [
         "draw" => !empty($_GET["draw"]) ? filter_input(INPUT_GET, "draw") : 0,
